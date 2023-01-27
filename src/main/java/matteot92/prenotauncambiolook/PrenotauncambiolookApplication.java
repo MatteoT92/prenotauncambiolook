812d7380a598efcf8560417639418b70a11aeedc
Matteo Tartaglione <61627873+MatteoT92@users.noreply.github.com>
2023-01-27 11:02:55 +0100

    Creazione Progetto Spring PrenotaUnCambioLook

---
+++ src/main/java/matteot92/prenotauncambiolook/PrenotauncambiolookApplication.java
@@ -0,0 +1,13 @@
+package matteot92.prenotauncambiolook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrenotauncambiolookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrenotauncambiolookApplication.class, args);
+	}
+
+}
