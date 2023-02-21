4d134f8b4a565b98efd9f14228f63c1cd6dfe118
Matteo Tartaglione <61627873+MatteoT92@users.noreply.github.com>
2023-02-21 14:32:24 +0100

    Aggiunta la possibilità, in caso di password dimenticata, di ricevere una temporanea per poter accedere (nel caso è verificata la presenza nel db

--- drop.sql
+++ drop.sql
@@ -94,3 +94,19 @@
     drop table servizi;
 
     drop table utenti;
+
+    drop table ordini;
+
+    drop table pagamenti;
+
+    drop table servizi;
+
+    drop table utenti;
+
+    drop table ordini;
+
+    drop table pagamenti;
+
+    drop table servizi;
+
+    drop table utenti;
--- src/main/resources/frontend/src/app/components/password/password.component.ts
+++ src/main/resources/frontend/src/app/components/password/password.component.ts
@@ -11,6 +11,7 @@ import { UtenteApiService } from 'src/app/services/utente-api.service';
 export class PasswordComponent {
 
   isAdmin = sessionStorage.getItem('tipo');
+  password!: string;
 
   constructor(private api: UtenteApiService, private router: Router) {
 
@@ -23,8 +24,21 @@ export class PasswordComponent {
   }
 
   onSubmitRecupera(recuperaPasswordForm: NgForm) {
+    let randomPassword = Math.random().toString(36).slice(-8); // genera una password casuale per poter accedere
     this.api.recuperaPassword(recuperaPasswordForm.value.username, recuperaPasswordForm.value.email)
-    .subscribe();
+    .subscribe(
+      (data) => {
+        if (data != null || data != undefined) {
+          sessionStorage.setItem('utente', recuperaPasswordForm.value.username);
+          this.api.modificaPassword(randomPassword)
+          .subscribe(
+            (data) => {
+              this.password = randomPassword;
+            }
+          );
+        }
+      }
+    );
   }
 
 }
