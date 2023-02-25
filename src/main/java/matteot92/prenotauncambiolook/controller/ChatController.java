71447c796fbc47a8d8a940b18d69811b3b1db69f
Matteo Tartaglione <61627873+MatteoT92@users.noreply.github.com>
2023-02-25 14:33:37 +0100

    Aggiunto cross-origin in ChatController Spring e leggere modifiche css in component servizi/right-menu

--- src/main/java/matteot92/prenotauncambiolook/controller/ChatController.java
+++ src/main/java/matteot92/prenotauncambiolook/controller/ChatController.java
@@ -1,25 +1,19 @@
 package matteot92.prenotauncambiolook.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 import matteot92.prenotauncambiolook.model.entities.ChatMessage;
 import matteot92.prenotauncambiolook.model.entities.MessageToSend;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-//@Controller
 @RestController
-@SessionAttributes({"username"})
 @CrossOrigin(origins = "http://localhost:4200")
 public class ChatController {
-	
-	// UTENTE CLIENTE
 
 	/**
 	 * Metodo che consente ad un utente cliente di poter inviare un messaggio
@@ -27,6 +21,7 @@ public class ChatController {
 	 */
     @MessageMapping("/chat")
     @SendTo("/topic/chat")
+    @CrossOrigin(origins = "http://localhost:4200/chat")
     public MessageToSend send(ChatMessage message) throws Exception {
 
         MessageToSend outMessage = new MessageToSend();
@@ -35,22 +30,5 @@ public class ChatController {
         outMessage.setTime(new SimpleDateFormat("HH:mm dd-MM-yyyy").format(new Date()));
         return outMessage;
     }
-    
-    // UTENTE ADMIN
-    
-    /**
-	 * Metodo che consente ad un utente admin di poter inviare un messaggio
-	 * sul canale topic/chat utilizzando il WebSocket
-	 */
-    @MessageMapping("/admin/chat")
-    @SendTo("/topic/chat")
-    public MessageToSend sendAdmin(ChatMessage message) throws Exception {
 
-        MessageToSend outMessage = new MessageToSend();
-        outMessage.setSender(message.getSender());
-        outMessage.setText(message.getText());
-        outMessage.setTime(new SimpleDateFormat("HH:mm dd-MM-yyyy").format(new Date()));
-        return outMessage;
-    }
-    
 }
